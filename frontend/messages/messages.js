16b555e0333a9025ca8f3999ee4a382705651993
Brandon Heenan <bmheenan@outlook.com>
2017-05-24 09:25:55 -0700

    Show messages in frontend
    Messages are now displayed in the main pain

--- frontend/messages/messages.js
+++ frontend/messages/messages.js
@@ -1,38 +1,77 @@
 messages();
 
 function messages() {
-	/*ajax({
-		type: "GET",
-		url: "api/test-hello",
-		sendToken: true,
-		callback: populateHelloMessage
-	});*/
+	// Public
+	globals.messages_showMessages = showMessages;
+
+	// Set up UI
 	$(".newMessageContainer .collapsed").addEventListener("click", expandNewMessageDiv);
 	$(".newMessageContainer .hideButton").addEventListener("click", collapseNewMessageDiv);
+	$(".newMessageContainer .submitButton").addEventListener("click", submitMessage);
 
-	/*function populateHelloMessage(error, response) {
-		if (error) {
-			console.log("Error trying to get hello message");
-			console.log(error);
-			return;
-		}
-		var allMessages = "";
-		var message = JSON.parse(response).message;
-		for (var i = 0; i < 100; i++) {
-			allMessages += "<p>" + message + "</p>";
-		}
-		$(".messages .allMessages").innerHTML = allMessages;
-	}*/
-
-	function expandNewMessageDiv(event) {
+	function expandNewMessageDiv(e) {
 		$(".newMessageContainer .expanded").classList.remove("hidden");
 		$(".newMessageContainer .collapsed").classList.add("hidden");
 		$(".messages .allMessages").classList.add("obscured");
 	}
 
-	function collapseNewMessageDiv(event) {
+	function collapseNewMessageDiv(e) {
 		$(".newMessageContainer .expanded").classList.add("hidden");
 		$(".newMessageContainer .collapsed").classList.remove("hidden");
 		$(".messages .allMessages").classList.remove("obscured");
+	}
+
+	// Show a channel
+	function showMessages(path) {
+		ajax({
+			type: "POST",
+			url: "api/message-get-list",
+			sendToken: true,
+			data: {
+				path: path
+			},
+			callback: showMessagesFromCallback
+		});
+	}
+
+	function showMessagesFromCallback(error, response) {
+		if (error) {
+			$(".messages .allMessage").innerHTML = "Sorry, something went wrong";
+			console.log(error);
+			return;
+		}
+		var messages = JSON.parse(response);
+		var messagesDiv = $(".messages .allMessages");
+		messagesDiv.innerHTML = "";
+		for (var i = 0; i < messages.length; i++) {
+			var message = document.createElement("div");
+			message.innerHTML = `${messages[i].author}: ${messages[i].text}`;
+			messagesDiv.appendChild(message);
+		}
+	}
+
+	// Send a message
+	function submitMessage(e) {
+		if (e) { e.preventDefault(); }
+		var input = $(".messages .newMessageContainer .newMessageTextInput");
+		ajax({
+			type: "POST",
+			url: "api/message-new",
+			sendToken: true,
+			data: {
+				author: globals.username,
+				text: input.value,
+				path: globals.path
+			},
+			callback: submitMessageCallback
+		});
+		input.value = "";
+	}
+
+	function submitMessageCallback(error, response) {
+		collapseNewMessageDiv();
+		setTimeout(function() {
+			showMessages(globals.path);
+		}, 250);
 	}
 }
